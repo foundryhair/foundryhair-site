5355c4b90809fb831a3853b05ec679c0706eb5f3
Taylor Ho <taylorkmho@gmail.com>
2016-03-14 02:28:35 -0700

    Responsive work for instagram feed

--- src/assets/js/application.js
+++ src/assets/js/application.js
@@ -11,11 +11,23 @@ $(function(){
   /*
     INSTAGRAM LOADING
   */
-  if ($('#instagram-feed').length && !Modernizr.touch) {
+  if ($('.instagram-feed').length) {
+    var count,
+        target;
+    var mqDesktop = window.matchMedia("screen and (min-width: 768px)");
+    if (mqDesktop.matches) {
+      count  = 20;
+      target = '.instagram-feed--desktop';
+    } else {
+      count  = 8;
+      target = '.instagram-feed--mobile';
+    }
     $.getJSON('https://api.instagram.com/v1/users/2150537699/media/recent/?client_id=d6df1ba940414fbfbbc3c85dce7a0d06&callback=?',function (insta) {
-      $.each(insta.data,function (photos,src) {
-        if ( photos === 20 ) { return false; }
-        $('<div class="instagram-image" style="background-image:url('+ src.images.standard_resolution.url +'"></div>').appendTo('#instagram-feed');
+      $.each(insta.data,function (index,src) {
+        if ( index === count ) {
+          return false;
+        }
+        $('<div class="instagram-image" style="background-image:url('+ src.images.standard_resolution.url +')"></div>').appendTo(target);
       });
     });
   }
