cb5db8f82db88f1f26e6b97d0c7da6a33c5b29d9
Taylor Ho <taylorkmho@gmail.com>
2016-04-03 18:37:16 -0700

    Added instafeed, removed jQuery

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,18 @@
 require('es6-shim');
+
 import MapGen from "./lib/map-generator";
 
 window['map'] = new MapGen('#map', 'map.mapInit');
 
+// let instagramFeed = new InstagramFeed('.feed', 'd6df1ba940414fbfbbc3c85dce7a0d06');
+let instagramFeed = new Instafeed({
+    get: 'user',
+    userId: '2150537699',
+    clientId: 'd6df1ba940414fbfbbc3c85dce7a0d06',
+    resolution: 'standard_resolution',
+    template: '<div class="feed__image" style="background-image:url({{image}})"></div>'
+});
+instagramFeed.run();
 // $(function(){
 
 //   var copyrightCurrentYear = $('.copyright').find('span').html();
