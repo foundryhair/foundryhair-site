08d3186f93748ce8662b0db5eba04bf151ec49e7
Taylor Ho <taylorkmho@gmail.com>
2016-06-17 17:23:55 -0700

    Update 2016-06-18T00:23:53.336Z

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,12 +38,11 @@ var InstagramFeed = function () {
 
     this.selector = selector;
     this.domSelector = document.querySelector('#' + selector);
-
     this.instagramOptions = {
       target: this.selector,
       get: 'user',
       userId: '2150537699',
-      clientId: 'd6df1ba940414fbfbbc3c85dce7a0d06',
+      accessToken: '2150537699.0859844.afc93a81b3bb4c0c9a3fa70de5740b9a',
       resolution: 'standard_resolution',
       sortBy: 'most-liked',
       template: '<div class="feed__image" style="background-image:url({{image}})"></div>'
